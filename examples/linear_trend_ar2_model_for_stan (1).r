ec839db755745eeb06ca9a02d0558763a62daf7c
jrnold <jeffrey.arnold@gmail.com>
2016-06-30 21:14:16 -0700

    working on example for mailing list

--- examples/linear_trend_ar2_model_for_stan (1).r
+++
@@ -1,8 +0,0 @@
-training_data_list = dget("training_data_list.txt")
-
-fit.stan2 <- stan (file = "linear_trend_ar2_model.stan",
-                   data = list (N = length (training_data_list),
-                                y = matrix (c (training_data_list), nrow = 1)),
-                   # pars = c ( "V", "W"),
-                   chains = 4,
-                   iter = 2000, warmup = 1000, thin = 1)

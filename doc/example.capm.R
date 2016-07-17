a3912f3c4af4c64dce88f7164c7be939dd98dea4
jrnold <jeffrey.arnold@gmail.com>
2016-07-17 00:55:22 -0700

    removing accidental file

--- doc/example.capm.R
+++
@@ -1,6 +0,0 @@
-## CAPM
-
-This is a simplified example of CAPM model from Petris (2011) using data from Berndt (1991).
-
-A simplified capital-asset pricicing model (CAPM) model estimates the relationship
-between the excess returns of a stock

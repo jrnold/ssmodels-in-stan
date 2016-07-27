486f8933728ab7d5ff9aadefd4fecde4e8ff200f
jrnold <jeffrey.arnold@gmail.com>
2016-07-26 20:56:07 -0700

    Update test coverage script
    Read a file that includes the tests for files that include multiple tests.

--- scripts/checktests.py
+++ scripts/checktests.py
@@ -1,17 +1,31 @@
+"""
+Check the coverage of tests for Stan functions.  If determines which functions
+are covered from
+
+- names of the test-*.R
+- functions listed in tets.yaml: since some models test multiple functions, especially the ones that test the extractor functions.
+
+"""
 import sys
 import os
 from os import path
 import re
 
+import yaml
+
 from standoc import parse_stan_function_defs
 
 def check_functions(filename, testdir):
     tests = []
+    with open(path.join(testdir, 'tests.yaml'), 'r') as f:
+        known_tests = yaml.load(f)
+    for k, v in known_tests.items():
+        for tst in v:
+            tests.append(tst)
     for f in os.listdir(testdir):
         m = re.match(r'^test-(.*)\.R$', f)
         if m:
             tests.append(m.group(1))
-    print(tests)
     with open(filename, 'r') as f:
         functions = parse_stan_function_defs(f.read())
     missing = []
